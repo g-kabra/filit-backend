fe164f24a09636a05cbeae2b79bd2bbef7bf658c
Gaurav Kabra <gauravkab07@gmail.com>
2023-03-02 18:37:46 +0530

    First deployment

--- filitmvpbackend/filitmvpbackend/settings.py
+++ filitmvpbackend/filitmvpbackend/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-111b7%#wf2ff83h7jltp7!ed_46hosar9(x#d0fo@jinc#9s##"
+SECRET_KEY = "111b7%#wf2ff83h7jltp7!ed_46hosar9(x#d0fo@jinc#9s##"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "filitmvp.azurewebsites.net/",
+]
 
 
 # Application definition
